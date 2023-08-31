8860b026c60226e1f0983606c2d7a41eb85dba52
ouchi <misaki.ouchi17@gmail.com>
2023-08-31 18:28:26 +0900

    途中経過

---
+++ app.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const port = 3000;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.set("view engine", "ejs");
+
+const mysql = require("mysql2");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "calender",
+  multipleStatements: true,
+});
+
+// cssファイルの取得
+app.use(express.static("assets"));
+
+app.get("/", (req, res) => {
+  const sql = "SELECT * from schedule;";
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    res.render("index", {
+      schedule: result
+    });
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
